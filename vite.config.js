3a6e87be86dbcf5453fdb7124670116681d5d46b
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2024-03-30 22:26:25 +0530

    .env secure

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{'/':'https://creator07portfolio.netlify.app'}
-  },
+  // server:{
+  //   proxy:{'/':'https://creator07portfolio.netlify.app'}
+  // },
   plugins: [react()],
 })
